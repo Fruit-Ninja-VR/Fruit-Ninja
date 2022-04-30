43661a5b563da8d1b3b661d5e7440b9149765ab0
Paddy Murray <g00344530@gmit.ie>
2022-04-30 20:45:39 +0100

    Added basic fruit spawner that destroys fruit also

--- Assets/_Scripts/FruitCutter.cs
+++ Assets/_Scripts/FruitCutter.cs
@@ -14,8 +14,12 @@ public class FruitCutter : MonoBehaviour
         GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
 
         if(!pieces[1].GetComponent<Rigidbody>())
+        {
             pieces[1].AddComponent<Rigidbody>();
-
-        Destroy(pieces[1], 1);
+            MeshCollider temp = pieces[1].AddComponent<MeshCollider>();
+            temp.convex = true;
+        }
+            
+        //Destroy(pieces[1], 1);
     }
 }
---
+++ Assets/_Scripts/FruitSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    public GameObject[] fruit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnFruit());
+    }
+
+    IEnumerator SpawnFruit()
+    {
+        while(true) 
+        {
+            GameObject go = Instantiate(fruit[Random.Range(0, fruit.Length)]);
+            Rigidbody temp = go.GetComponent<Rigidbody>();
+
+            temp.velocity = new Vector3(0f, 5f, .5f);
+            temp.angularVelocity = new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f));
+            temp.useGravity = true;
+
+            Vector3 pos = transform.position;
+            pos.x += Random.Range(-1f, 1f);
+            go.transform.position = pos;
+
+            yield return new WaitForSeconds(1f);    
+        }
+    }
+}
