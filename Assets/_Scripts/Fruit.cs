e38ab92e6f22751e45fb3b6b9c57d705bfe78f90
Paddy Murray <g00344530@gmit.ie>
2022-05-03 20:01:19 +0100

    Updated vibrations and stopped player hitting bomb multiple times

--- Assets/_Scripts/Fruit.cs
+++ Assets/_Scripts/Fruit.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 { 
+    bool hit = false;
     // Update is called once per frame
     void Update()
     {
         if(transform.position.y < -10f)
             Destroy(this.gameObject);
+    }
+
+    public void GotHit(bool gotHit) 
+    {
+        this.hit = gotHit;
+    }
+
+    public bool WasHit()
+    {
+        return this.hit;
     }
 }
